6aebdd43e7853d2536aba92c4864f9bd8a0f671a
RicardoCQB <ricardo_cqb@hotmail.com>
2021-02-17 10:56:20 +0000

    added trigger labels to the EOG signal

--- src/visualization.py
+++ src/visualization.py
@@ -2,13 +2,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def plotEogElectrodesSignal(signal, start=0, end=1000, labels=[],
                   xAxisLabel='Datapoint',
                   yAxisLabel='Amplitude (microVolts)'):
     '''
-    Method for plotting the EOG with any number of electrodes read from EDF file using the method in the reading section
+    Function for plotting the EOG with any number of electrodes read from EDF file using the method in the reading section
     in the jupyter notebook.
     :param signal: signal to be plotted
     :param start: index of the start of the plot
@@ -28,10 +29,21 @@ def plotEogElectrodesSignal(signal, start=0, end=1000, labels=[],
     plt.show()
 
 def plotVertHorEOG(verticalEOG, horizontalEOG, start, end, mode='both'):
+    '''
+    Function for plotting the vertical and horizontal EOG signals, these signals are the ones that will be used to
+    detect and classify saccades.
+    :param verticalEOG: vertical signal of the EOG, that means the up electrode values minus the down electrode valus
+    :param horizontalEOG: horizontal signal of the EOG, left electrode signal values minus the right electrode signal values
+    :param start: integer index of the start of the plot
+    :param end: integer index of the end of the plot
+    :param mode: the mode chooses if it is plotted the vertical EOG or the horizontal EOG or both
+    Therefore, there are three modes: 'both', 'vertical' and 'horizontal'
+    :return:
+    '''
     verticalEOG = verticalEOG[start:end]
     horizontalEOG = horizontalEOG[start:end]
 
-    plt.figure(figsize=(20, 5))
+    plt.figure(figsize=(40, 10))
     if mode == 'both':
         plt.plot(verticalEOG, color='cyan')
         plt.plot(horizontalEOG, color='magenta')
@@ -46,6 +58,17 @@ def plotVertHorEOG(verticalEOG, horizontalEOG, start, end, mode='both'):
     plt.xlabel('Datapoint')
     plt.ylabel('Amplitude (microVolts)')
     plt.legend(labels)
+
+    # Reading the .csv file that contains the triggers for this .edf signal file
+    triggerCsv = pd.read_csv('C:/Users/Ricardo\source/enac-eog-analysis/data/EOG_EyeLink/RI02/labels_triggers_602.csv')
+
+    #print(triggerCsv)
+
+    for triggerPoint, triggerLabel in zip(triggerCsv['latency'], triggerCsv['type']):
+        if triggerPoint >= start and triggerPoint <= end:
+            plt.axvline(x=triggerPoint-start, color='black')
+            plt.text(triggerPoint-start, 0, triggerLabel, rotation='vertical')
+
     plt.show()
 
 
